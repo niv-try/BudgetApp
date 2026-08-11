6b02bb9e80816e2060c1c2760ca79eb0bcc89165
Niv <csgoniv@gmail.com>
2026-08-11 20:49:40 +0300

    fixing money

--- src/main/java/com/example/budgetapp/AccountController.java
+++ src/main/java/com/example/budgetapp/AccountController.java
@@ -3,6 +3,7 @@ package com.example.budgetapp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/account")
@@ -12,11 +13,33 @@ public class AccountController {
     @Autowired
     private AccountRepository repository;
 
+    @Autowired
+    private TransactionRepository transactionRepository;
+
     @GetMapping
     public Account getBalance() {
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
-        return repository.findByOwnerUsername(username)
-                .orElse(new Account(username, 0.0)); // אם אין חשבון, מחזיר 0
+
+        // 1. שליפת העו"ש/יתרת הבסיס שהוגדרה ידנית
+        Account account = repository.findByOwnerUsername(username)
+                .orElse(new Account(username, 0.0));
+        double currentBalance = account.getBalance();
+
+        // 2. שליפת כל העסקאות של המשתמש וחישובן
+        List<Transaction> transactions = transactionRepository.findByOwnerUsername(username);
+        for (Transaction t : transactions) {
+            // אם זו הכנסה, מוסיפים לסכום
+            if ("income".equals(t.getType())) {
+                currentBalance += t.getAmount();
+            }
+            // אם זו הוצאה או חיסכון, מורידים מהסכום
+            else if ("expense".equals(t.getType()) || "savings".equals(t.getType())) {
+                currentBalance -= t.getAmount();
+            }
+        }
+
+        // מחזירים אובייקט עם הסכום המשוקלל
+        return new Account(username, currentBalance);
     }
 
     @PostMapping
@@ -24,6 +47,7 @@ public class AccountController {
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
         Account account = repository.findByOwnerUsername(username)
                 .orElse(new Account(username, 0.0));
+
         account.setBalance(newBalance);
         return repository.save(account);
     }
